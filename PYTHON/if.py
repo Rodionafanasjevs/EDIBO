4d36c87314d51b72077bc5299d8c54eddd8912f3
Rodionafanasjevs <67696809+Rodionafanasjevs@users.noreply.github.com>
2020-07-25 10:41:35 +0300

    Add files via upload

---
+++ PYTHON/if.py
@@ -0,0 +1,100 @@
+# cars = ['audi','toyota','mercedes-benz','bmv','opel']
+# for car in cars:
+#     if car == 'bmv':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# Python3
+
+# answer = 21
+# if answer != 32:
+#     print("This is not correct answer")
+
+# age_0 = 21
+# age_1 = 23
+# age_0 <= 25 and age_1 >= 20
+
+# toopings = ['blue chees', 'beef', 'chees souce', 'tomatoes', 'mozzarela']
+# 'beef' in toopings
+
+# game_active = True
+# can_edit = False
+
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+
+# print("\nIs car == 'audi'? I predict False")
+# print(car == 'audi')
+
+# age = 17
+# if age >= 21:
+#     print("\nSorry, you are old enouhg to vote!")
+# else:
+#     print("\nWelcome, you can regist")
+
+# age = (66)
+# if age < 9:
+#     price = 0
+# elif age < 18:
+#     price = 30
+# elif age >= 65:
+#     price = 30
+# else:
+#     price = 40
+
+# print(f"Your admission cost is ${price}.")
+
+# После одной принятой команды if, команду elif python игнорируетю.
+
+# peperoni = ['mashrooms', 'cheese', 'souce', 'salami']
+# if 'mashrooms' in peperoni:
+#     print("Adding mashrooms")
+# if 'tomatoes' in peperoni:
+#     print("Adding tomatoes")
+# if 'salami' in peperoni:
+#     print("Adding salami")
+# if 'souce' in peperoni:
+#     print("Adding souce")
+# if 'checken' in peperoni:
+#     print("Adding checken")
+# if :
+#     print("Adding cheese")
+# print("Your pizza is done")
+
+# or
+
+# pepperoni = ['mashrooms', 'cheese', 'souce', 'salami']
+# for item in peperoni:
+#     if item in peperoni:
+#         print(f'Adding {item}!')
+
+#     if item == 'mashrooms':
+#         print("Sorry, we are out of mashrooms right now.")
+#     else:
+#         print(f"Adding {item}")
+# print("Pizza well done")
+
+# available_toppings = ['pepperoni', 'cheese', 'blue cheese', 'olive', 'mashrooms', 'extra cheese']
+# requested_toppings = ['cheese', 'squid', 'pepperoni']
+# for requested_topping in requested_toppings:
+#     if requested_topping in available_toppings:
+#         print(f"Adding {requested_topping}.")
+#     else:
+#         print(f"Sorry, we don't have {requested_topping}")
+# print("\nFinished making your pizza!")
+
+# guests = ['eduards', 'lukas', 'admin', 'karlis', 'sandra']
+# for guest in guests:
+#     if guest == 'admin':
+#         print(f"Hello {guest}, would you like to see a status report?")
+#     else:
+#         print(f"Hello {guest.title()}, thank you for logging in again.")
+
+# guests = [[]]
+# for guest in guests:
+#     if guest == 'admin':
+#         print(f"Hello {guest}")
+#     if guest == []:
+#         print("We need to ind some user!")
